22ffb15c00dbb81007edf4ea43939b34d04a65a8
isoboye24 <isobarguy4real@gmail.com>
2026-02-16 00:17:49 +0100

    Implemented create DB is not exist on first run.

--- RoutineAPP/Program.cs
+++ RoutineAPP/Program.cs
@@ -1,22 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.EntityClient;
+using System.Data.SqlClient;
+using System.IO;
 using System.Windows.Forms;
 
 namespace RoutineAPP
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            bool databaseWasCreated;
+            string databaseName = EnsureDatabaseExists(out databaseWasCreated);
+
+            if (databaseWasCreated)
+            {
+                RunSchemaScript(databaseName);
+            }
+
             Application.Run(new FormDashboard());
+        }
+
+        private static string EnsureDatabaseExists(out bool created)
+        {
+            created = false;
+
+            string efConnection = ConfigurationManager
+                .ConnectionStrings["RoutineDBEntities"]
+                .ConnectionString;
+
+            EntityConnectionStringBuilder entityBuilder =
+                new EntityConnectionStringBuilder(efConnection);
+
+            string sqlConnectionString = entityBuilder.ProviderConnectionString;
+
+            SqlConnectionStringBuilder sqlBuilder =
+                new SqlConnectionStringBuilder(sqlConnectionString);
+
+            string databaseName = sqlBuilder.InitialCatalog;
+            string dataSource = sqlBuilder.DataSource;
+
+            string masterConnection =
+                $"Data Source={dataSource};Initial Catalog=master;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(masterConnection))
+            {
+                connection.Open();
+
+                string checkQuery =
+                    $"IF DB_ID('{databaseName}') IS NULL BEGIN CREATE DATABASE [{databaseName}]; SELECT 1 END ELSE SELECT 0";
+
+                using (SqlCommand cmd = new SqlCommand(checkQuery, connection))
+                {
+                    int result = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (result == 1)
+                        created = true;
+                }
+            }
+
+            return databaseName;
+        }
+
+        private static void RunSchemaScript(string databaseName)
+        {
+            string scriptPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "database",
+                "CreateDatabase.sql");
+
+            if (!File.Exists(scriptPath))
+                return;
+
+            string script = File.ReadAllText(scriptPath);
+
+            string connectionString =
+                $"Data Source=localhost\\SQLEXPRESS;Initial Catalog={databaseName};Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(script, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
