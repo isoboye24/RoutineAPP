67a7ce67fae1a2bef4282fc42f4998942c19e687
isoboye24 <isobarguy4real@gmail.com>
2026-02-20 10:53:08 +0100

    Task Helper Service

---
+++ RoutineAPP/HelperService/TaskHelperService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoutineAPP.HelperService
+{
+    public class TaskHelperService
+    {
+        public enum TaskGridType
+        {
+            Basic
+        }
+
+        public static void ConfigureTaskGrid(DataGridView grid, TaskGridType type)
+        {
+            switch (type)
+            {
+                case TaskGridType.Basic:
+                    GeneralHelperService.SetVisibleColumns(grid, "CategoryName", "TimeSpent", "TimeInHoursAndMinutes");
+                    GeneralHelperService.RenameColumns(grid, new Dictionary<string, string>
+                                {
+                                    { "CategoryName", "Category" },
+                                    { "TimeSpent", "In Minute" },
+                                    { "TimeInHoursAndMinutes", "In Hour" },
+                                });
+                    break;
+            }
+        }
+    }
+}
